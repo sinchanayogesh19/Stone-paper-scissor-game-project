96ac617e4ba2988bfbddcb8560247700d47d53ac
sinchanayogesh19 <sinchanaayogesh@gmail.com>
2026-06-02 14:49:19 +0530

    Remove commented code and log heading element

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-//let heading = document.getElementById("heading");
-//console.dir(heading);
 
-
-let headings=document.getElementById("heading-class");
-console.dir(headings);
-console.log(headings);
