5ffffeac4d9ffd82580c4119cbcceea01948135d
sinchanayogesh19 <sinchanaayogesh@gmail.com>
2026-06-02 13:00:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//let heading = document.getElementById("heading");
+//console.dir(heading);
+
+
+let headings=document.getElementById("heading-class");
+console.dir(headings);
+console.log(headings);
